ae3c300e19527f58dcbc2f0a20e27c129ed7712e
Mohammed Razik M <150541743+MohammedRazik-M@users.noreply.github.com>
2025-05-14 23:39:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import AdviceApp from './AdviceApp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <AdviceApp />
+    </>
+  )
+}
+
+export default App
